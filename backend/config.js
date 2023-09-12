8b475785fd8fbf60e14d072725394e7c22b3686a
Faith Chulu <chulufaith126@gmail.com>
2023-09-12 18:46:51 +0200

    Update to config.js

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
 export const PORT = 5555;
+
+export const mongoDBURL = `mongodb+srv://root:<Chimwemwe@cluster0.bhxezut.mongodb.net/books-collection?retryWrites=true&w=majority`
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,28 @@
 import express  from "express";
-import { PORT } from "./config.js";
-
+import { PORT,mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
-app.listen(PORT, () => {
-    console.log(`App is listening on port: ${PORT}`)
-})
+
+app.get('/', (request, response)=>{
+   
+    console.log(request)
+    return response.status(243).send('Welcome to mern stack tutorial!');
+
+});
+
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(()=>{
+        console.log(`App connected to database!`)
+    
+        app.listen(PORT, () => {
+            console.log(`App is listening on port: ${PORT}`)
+        })
+    
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
