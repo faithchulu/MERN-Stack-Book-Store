27e624c1b74f6849f677672056811c9b7e776624
Faith Chulu <chulufaith126@gmail.com>
2023-09-13 16:43:19 +0200

    Created book model

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,39 @@
 import express  from "express";
 import { PORT,mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
+import { Book } from "./models/bookModel.js";
+
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/', (request, response)=>{
-   
     console.log(request)
     return response.status(243).send('Welcome to mern stack tutorial!');
-
 });
 
+//Route to save a new book
+app.post('/books',async (request, response)=>{
+try{
+if (!response.body.title || !response.body.author || !response.body.publishYear){
+return response.status(400).send({message: 'Send all required fields: title, author, publishYear!'});
+}
+ 
+const newBook = {
+    title: response.body.title,
+    author: response.body.author,
+    publishYear: response.body.publishYear,
+};
+
+const book = await Book.create(newBook);
+
+return response(201).send(book);
+}catch(error){
+console.log(error.message);
+response.status(500).send({message: error.message});
+}
+})
 
 
 mongoose
