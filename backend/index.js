ad0422a0d461dd9a1037c8c03c5d9263be3fa931
Faith Chulu <chulufaith126@gmail.com>
2023-09-15 08:53:55 +0200

    Updated CORS middleware

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 //Middleware for handling CORS policy 
 //app.use(cors());
 
-//
+//MIddleware for handling CORS policy specifying one origin
 cors({
     origin: 'http://localhost:3000',
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
